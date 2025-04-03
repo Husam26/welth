e73b189b5da5484bc72ece768a7915f9f20312a1
Mohd Husamuddin <mohdhusamuddin2005@email.com>
2025-04-03 22:30:29 +0530

    recheck 1

--- middleware.js
+++ middleware.js
@@ -46,9 +46,6 @@ export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next|.*\\..*).*)", // Matches all pages except static files (_next, .js, .css, etc.)
   ],
 };
